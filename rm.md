dba5230d02f55b54a93188a80ff3d2aa94289add
AhmedMGod <ahmed_mumin.god@outlook.de>
2025-07-02 14:50:26 +0200

    Add FizzBuzz task and readme file

---
+++ rm.md
@@ -0,0 +1,11 @@
+# FizzBuzz-Task
+
+In a language of your choice write a program that prints the numbers from 1 to 100.
+###But for multiples of three print ***Fizz*** instead of the number and for the multiples of five print
+***Buzz***.
+For numbers which are multiples of both three and five print ***FizzBuzz***.
+While coding please keep in mind the following aspects:
+- easy maintenance
+- adaptability to new requirements
+- version tracking
+- the code is to be supported by a team
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        for (int i = 1; i <= number; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i + "");
+            }
+
+        }
+    }
+}
